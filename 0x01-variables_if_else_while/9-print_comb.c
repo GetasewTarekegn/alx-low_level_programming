e2feae10594dc8ea075bdf5f7a5c7dcb1c40b49e
Getasew Tarekegn <getasewgech2009@gmail.com>
2022-03-11 09:07:40 +0000

    Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,6 +6,16 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
-
-int main(voi
+ *     **/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');														putchar(' ');														}}
+putchar('\n');
+return (0);
+}
