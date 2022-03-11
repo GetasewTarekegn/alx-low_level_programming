aa9dd415eb0b75afae42d8d50c517902a09c80b2
Getasew Tarekegn <getasewgech2009@gmail.com>
2022-03-11 10:31:00 +0000

    combination of single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     **/
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * **/
 int main(void)
 {
 int d;
@@ -15,7 +14,10 @@ for (d = '0'; d <= '9'; d++)
 putchar(d);
 if (d != '9')
 {
-putchar(',');														putchar(' ');														}}
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
