6325ddd8b14654dd20cf3f55385eed9436ef0761
Getasew Tarekegn <getasewgech2009@gmail.com>
2022-03-16 04:44:53 +0000

    task 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,24 @@
  *     **/
 void jack_bauer(void)
 {
-int H, M;
-for (H = 0; H < 24; H++)
+int h1, h2, m1, m2;
+for (h1 = 0; h1 <= 2; h1++)
 {
-for (M = 0; M < 60; M++)
+for (h2 = 0; h2 <= 9; h2++)
 {
-_putchar((H / 10) + '0');
-_putchar((H % 10) + '0');
+for (m1 = 0; m1 <= 5; m1++)
+{
+for (m2 = 0; m2 <= 9; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
 _putchar(':');
-_putchar((M / 10) + '0');
-_putchar((M % 10) + '0');
-_putchar('\n');	
+_putchar(m1 + '0');				
+_putchar(m2 + '0');
+_putchar('\n');  }
+}
+if (h2 == 3 && h1 == 2)
+break;
 }
 }
 }
