7559508703762b41f7ff4660ffe9fc5638f38a42
Getasew Tarekegn <getasewgech2009@gmail.com>
2022-03-16 04:02:54 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  *   * Description: It prints the word putchar, followed by a new line.
  *    * Return: 0.
  *     **/
-int main(void){
+int main(void)
+{
 char word[8] = "_putchar";
 int i;
 for  (i = 0; i < 8; i++)
